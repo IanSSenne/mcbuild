f4ba7ee776a1dc29e4e5f26e6b0576ad633b1233
IanSSenne <IanSSenne@gmail.com>
2020-09-17 03:13:04 -0700

    update readme

---
+++ config.js
@@ -0,0 +1,16 @@
+//generated config
+module.exports = {
+  "global": {
+    "onBuildSuccess": null
+  },
+  "mc": {
+    "dev": true,
+    "header": "#built using mc-build (https://github.com/IanSSenne/mcbuild)",
+    "internalScoreboard": "LANG_MC_INTERNAL",
+    "rootNamespace": null,
+    "lib": null
+  },
+  "mc-extra": {
+    "integrated": false
+  }
+}
